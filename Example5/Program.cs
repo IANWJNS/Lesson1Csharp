2447a2c5822fad50611bddbf74b19f94b4c67420
Maria <ixfel363@gmail.com>
2023-10-02 17:27:05 +0300

    ConditionEx5

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yahoo, it's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
